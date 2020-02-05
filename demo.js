236f097905f9ad45ac6c005ef0ed546b6a957648
niksein007 <markpearceniksein@gmail.com>
2020-02-05 11:19:58 +0000

    classwork

--- demo.js
+++ demo.js
@@ -1,4 +1,11 @@
-console.log(`mark`);
-const nonsense =()=>{
-    console.log(`git is shit`);
+const dataChecker = (string,rank)=>{
+if (string == "codenation" && rank == 1) {
+    console.log(`welcome to ${string} your have a rank of  ${rank}`)
+} else if(string == "codenation" && rank != 1) {
+    console.log(`welcome to ${string} your have an incorrect rank of ${rank}`)
+} {
+    
 }
+}
+        dataChecker("codenation",2);
+
